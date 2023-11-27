73abcf9f55de637065448bb5b97e21fe106e0613
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-11-27 22:44:26 +0200

    add migrations

---
+++ BlazorBlog.Data/Migrations/20231126164336_AddNameToAppUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameToAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
--- BlazorBlog/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-namespace BlazorBlog.Data
-{
-    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore;
-
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
