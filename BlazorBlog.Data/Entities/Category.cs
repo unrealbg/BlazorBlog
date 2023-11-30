e8cade7a1e6f46d819729c15b2fb5cc1439560a8
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-11-30 17:35:27 +0200

    add  seed categiries func

--- BlazorBlog.Data/Entities/Category.cs
+++ BlazorBlog.Data/Entities/Category.cs
@@ -13,5 +13,27 @@
         public string Slug { get; set; }
 
         public bool ShowOnNavBar { get; set; }
+
+        public static Category[] GetSeedCategories()
+        {
+            return new[]
+            {
+                new Category { Name = "Blazor", Slug = "blazor", ShowOnNavBar = true },
+                new Category { Name = "C#", Slug = "csharp", ShowOnNavBar = true },
+                new Category { Name = "CSS", Slug = "css", ShowOnNavBar = true },
+                new Category { Name = "Docker", Slug = "docker", ShowOnNavBar = false },
+                new Category { Name = "Git", Slug = "git", ShowOnNavBar = false },
+                new Category { Name = "HTML", Slug = "html", ShowOnNavBar = true },
+                new Category { Name = "JavaScript", Slug = "javascript", ShowOnNavBar = true },
+                new Category { Name = "Linux", Slug = "linux", ShowOnNavBar = false },
+                new Category { Name = "SQL", Slug = "sql", ShowOnNavBar = true },
+                new Category { Name = "Visual Studio", Slug = "visual-studio", ShowOnNavBar = false },
+                new Category { Name = "Windows", Slug = "windows", ShowOnNavBar = false },
+                new Category { Name = "Xamarin", Slug = "xamarin", ShowOnNavBar = true },
+                new Category { Name = "XAML", Slug = "xaml", ShowOnNavBar = true },
+                new Category { Name = "XML", Slug = "xml", ShowOnNavBar = true },
+                new Category { Name = "MVC", Slug = "mvc", ShowOnNavBar = true }
+            };
+        }
     }
 }
