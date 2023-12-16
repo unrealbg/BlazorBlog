a1538a6a54d30e3a3b336d7aab67cf8821460b5f
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-17 00:05:06 +0200

    Implement ClaimsPrincipal extensions for user identity retrieval

---
+++ BlazorBlog/Common/Extentions.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBlog.Common
+{
+    using System.Security.Claims;
+
+    public static class Extentions
+    {
+        public static string GetUserName(this ClaimsPrincipal principal) =>
+            principal.FindFirstValue(AppConstants.ClaimNames.FullName)!;
+
+        public static string GetUserId(this ClaimsPrincipal principal) =>
+            principal.FindFirstValue(ClaimTypes.NameIdentifier)!;
+    }
+}
