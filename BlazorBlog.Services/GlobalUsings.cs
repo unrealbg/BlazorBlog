5bbaca07799591a1e20e2d0f21cd55824385345d
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-17 18:22:39 +0200

    Cleaned Up Usings in GlobalUsings.cs

--- BlazorBlog.Services/GlobalUsings.cs
+++ BlazorBlog.Services/GlobalUsings.cs
@@ -4,6 +4,7 @@ global using BlazorBlog.Data;
 global using BlazorBlog.Data.Entities;
 global using BlazorBlog.Services.Contracts;
 global using BlazorBlog.Services.Settings;
+
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Configuration;
