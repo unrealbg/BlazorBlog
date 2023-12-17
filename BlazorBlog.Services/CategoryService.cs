900d4fd42f1fb500d55f43c0145a8399b470cc4f
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-17 18:21:50 +0200

    Implemented CategoryService and ICategoryService in BlazorBlog
    - Added CategoryService class to manage category-related operations.
    - Created ICategoryService interface defining methods for category management.
    - Integrated DbContextFactory for database context management within CategoryService.
    - Implemented methods for retrieving, saving, and finding categories by slug.

---
+++ BlazorBlog.Services/CategoryService.cs
@@ -0,0 +1,78 @@
+﻿namespace BlazorBlog.Services
+{
+    using Contracts;
+    using Common;
+
+    public class CategoryService : ICategoryService
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> contextFactory;
+
+        public CategoryService(IDbContextFactory<ApplicationDbContext> contextFactory)
+        {
+            this.contextFactory = contextFactory;
+        }
+
+        public async Task<Category[]> GetCategoriesAsync()
+        {
+            return await this.ExecuteOnContext(async context =>
+            {
+                var categories = await context.Categories
+                    .AsNoTracking()
+                    .ToArrayAsync();
+                return categories;
+            });
+        }
+
+        public async Task<Category> SaveCategoryAsync(Category category)
+        {
+            return await this.ExecuteOnContext(async context =>
+            {
+                if (category.Id == 0)
+                {
+                    if (await context.Categories
+                            .AsNoTracking()
+                            .AnyAsync(c => c.Name == category.Name))
+                    {
+                        throw new InvalidOperationException($"Category with the name {category.Name} already exist");
+                    }
+
+                    category.Slug = category.Name.ToSlug();
+                    await context.Categories.AddAsync(category);
+                    await context.SaveChangesAsync();
+                }
+                else
+                {
+                    if (await context.Categories
+                            .AsNoTracking()
+                            .AnyAsync(c => c.Name == category.Name && c.Id != category.Id))
+                    {
+                        throw new InvalidOperationException($"Category with the name {category.Name} already exist");
+                    }
+
+                    var dbCategory = await context.Categories.FindAsync(category.Id);
+
+                    dbCategory.Name = category.Name;
+                    dbCategory.ShowOnNavBar = category.ShowOnNavBar;
+
+                    category.Slug = dbCategory.Slug;
+                }
+
+                await context.SaveChangesAsync();
+                return category;
+            });
+        }
+
+        public async Task<Category?> GetCategoryBySlugAsync(string slug) =>
+            await ExecuteOnContext(async context =>
+                await context.Categories
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(c => c.Slug == slug)
+                );
+
+        private async Task<TResult> ExecuteOnContext<TResult>(Func<ApplicationDbContext, Task<TResult>> query)
+        {
+            await using var context = this.contextFactory.CreateDbContext();
+            return await query.Invoke(context);
+        }
+    }
+}
---
+++ BlazorBlog.Services/Contracts/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorBlog.Services.Contracts;
+
+public interface ICategoryService
+{
+    Task<Category[]> GetCategoriesAsync();
+
+    Task<Category> SaveCategoryAsync(Category category);
+
+    Task<Category?> GetCategoryBySlugAsync(string slug);
+}
