40b33cfb10ec6afb3bf2747fa86708ec0254b49f
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-17 02:01:23 +0200

    Add GlobalUsings

---
+++ BlazorBlog.Services/GlobalUsings.cs
@@ -0,0 +1,9 @@
+// Global using directives
+
+global using BlazorBlog.Data;
+global using BlazorBlog.Data.Entities;
+global using BlazorBlog.Services.Contracts;
+global using BlazorBlog.Services.Settings;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
