3706f383ead632d3f7af4a1e8ba2c3ced305d056
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-17 18:57:17 +0200

    Switched to DbContextFactory in Program.cs
    - Replaced AddDbContext with AddDbContextFactory for ApplicationDbContext.
    - Enhanced the application to use the factory pattern for creating DbContext instances.
    - Updated service registration to improve performance and scalability.

--- BlazorBlog/Program.cs
+++ BlazorBlog/Program.cs
@@ -33,8 +33,10 @@ namespace BlazorBlog
                 .AddIdentityCookies();
 
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            
+            builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+            
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
