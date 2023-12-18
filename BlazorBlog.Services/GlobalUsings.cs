7726ead0933e49d6a429919ec1a53444fdee6d81
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-19 01:54:10 +0200

    Expanded Global Usings in Project

--- BlazorBlog.Services/GlobalUsings.cs
+++ BlazorBlog.Services/GlobalUsings.cs
@@ -4,6 +4,8 @@ global using BlazorBlog.Data;
 global using BlazorBlog.Data.Entities;
 global using BlazorBlog.Services.Contracts;
 global using BlazorBlog.Services.Settings;
+global using BlazorBlog.Services.Models;
+global using BlazorBlog.Services.Common;
 
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
