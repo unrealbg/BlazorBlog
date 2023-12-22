a0bfbf6a9981634e45db1d5a5610734172c61072
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-23 00:38:30 +0200

    Refactored BlogPost Entity for Enhanced Flexibility and Validation
    - Made Image property optional in BlogPost.
    - Enforced category selection validation for CategoryId.
    - Enabled lazy loading for Category and User navigation properties.

--- BlazorBlog.Data/Entities/BlogPost.cs
+++ BlazorBlog.Data/Entities/BlogPost.cs
@@ -12,7 +12,7 @@
         [MaxLength(125)]
         public string Slug { get; set; }
 
-        [Required, MaxLength(100)]
+        [MaxLength(100)]
         public string Image { get; set; }
 
         [Required, MaxLength(500)]
@@ -21,6 +21,7 @@
         [Required]
         public string Content { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a valid category.")]
         public int CategoryId { get; set; }
 
         public string UserId { get; set; }
@@ -33,9 +34,9 @@
 
         public bool IsDeleted { get; set; }
 
-        public Category Category { get; set; }
+        public virtual Category Category { get; set; }
 
-        public ApplicationUser User { get; set; }
+        public virtual ApplicationUser User { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
