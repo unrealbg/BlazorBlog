4d6bf206f3e0172e273853c3eda0314405c32a7a
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-01-15 22:11:14 +0200

    Feat: Introduce DetailPageModel for BlogPost Detailing
    - Added new record type 'DetailPageModel' in BlazorBlog.Services.Models.
    - The model encapsulates a BlogPost and an array of related BlogPosts.
    - Includes a static method 'Empty' for generating an empty instance.
    - Added 'IsEmpty' property to check if the main BlogPost is null.
    - Aims to streamline data handling for blog post details and related posts in the application.

---
+++ BlazorBlog.Services/Models/DetailPageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorBlog.Services.Models;
+
+public record DetailPageModel(BlogPost BlogPost, BlogPost[] RelatedPosts)
+{
+    public static DetailPageModel Empty() => new(default, []);
+
+    public bool IsEmpty => BlogPost is null;
+}
