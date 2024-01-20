85509c15376fb0e09285ae9c116445a8ce04fe08
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-01-20 15:39:24 +0200

    Refactor Using Directives and Add Global Usings for Enhanced Code Clarity
    - Streamlined `@using` directives across components by adding Blazored.TextEditor and BlazorBlog.Services.Utilities.
    - Introduced global using directives for commonly used namespaces to reduce redundancy and improve readability.

--- BlazorBlog/GlobalUsings.cs
+++ BlazorBlog/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using BlazorBlog.Data.Entities;
 global using BlazorBlog.Data;
 global using BlazorBlog.Services.Common;
 global using BlazorBlog.Services.Contracts;
+global using BlazorBlog.Services.Utilities;
 
 global using Microsoft.AspNetCore.Authentication;
 global using Microsoft.AspNetCore.Components;
@@ -12,6 +13,10 @@ global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Components.Forms;
 global using Microsoft.AspNetCore.Components.QuickGrid;
 global using Microsoft.AspNetCore.Identity;
+
+global using Ganss.Xss;
+
+global using Blazored.TextEditor;
 
 global using System.ComponentModel.DataAnnotations;
 global using System.Security.Claims;
