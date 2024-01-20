bc76ecc12dacf881162972f1778e9338a58d0cd3
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-01-20 16:52:31 +0200

    Improve Category Management with Confirmation Modals and Toast Notifications
    - Integrated a confirmation modal for category deletion in the ManageCategories component to provide a more intuitive user experience.
    - Added toast notifications for successful category additions, updates, and deletions, enhancing user feedback.
    - Refactored deletion logic to utilize the confirmation modal, ensuring user intent before proceeding with category removal.
    - Streamlined category editing and saving functionalities, making the UI more responsive and user-friendly.
    - Improved the overall robustness of category management, making the process clearer and more reliable for administrators.

--- BlazorBlog/Components/Pages/Admin/ManageCategories.razor.cs
+++ BlazorBlog/Components/Pages/Admin/ManageCategories.razor.cs
@@ -5,16 +5,21 @@
         private bool _isLoading;
         private string? _loadingText;
 
+        private bool _showConfirmationModal = false;
+
         private Category? _operatingCategory;
         private IQueryable<Category> _categories = Enumerable.Empty<Category>().AsQueryable();
 
         protected override async Task OnInitializedAsync() => await LoadCategoriesAsync();
 
-        [Inject] 
+        [Inject]
         ICategoryService CategoryService { get; set; }
 
-        [Inject] 
+        [Inject]
         NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        IToastService ToastService { get; set; }
 
         private async Task HandleShowOnNavBarChanged(Category category)
         {
@@ -35,6 +40,10 @@
                 _loadingText = "Saving changes";
                 _isLoading = true;
                 await CategoryService.SaveCategoryAsync(_operatingCategory);
+
+                var operation = _operatingCategory.Id == 0 ? "added" : "updated";
+                ToastService.ShowToast(ToastLevel.Success, $"Category {operation} successfully.", heading: "Success");
+
                 _operatingCategory = null;
                 _isLoading = false;
 
@@ -48,6 +57,54 @@
             _isLoading = true;
             _categories = (await CategoryService.GetCategoriesAsync()).AsQueryable();
             _isLoading = false;
+        }
+
+        private void ConfirmDeleteCategory(Category category)
+        {
+            _operatingCategory = category;
+            _showConfirmationModal = true;
+        }
+
+        private async Task DeleteCategoryAsync(int id)
+        {
+            if (_operatingCategory is not null)
+            {
+                _loadingText = "Deleting category";
+                _isLoading = true;
+                var isDeleted = await CategoryService.DeleteCategoryAsync(id);
+
+                if (!isDeleted)
+                {
+                    ToastService.ShowToast(ToastLevel.Error, "Failed to delete category", heading: "Error");
+                    _isLoading = false;
+                    return;
+                }
+
+                ToastService.ShowToast(ToastLevel.Success, "Category deleted successfully.", heading: "Success");
+
+                _operatingCategory = null;
+
+                _isLoading = false;
+
+                await LoadCategoriesAsync();
+            }
+        }
+
+        private async Task OnModalConfirm(bool isConfirmed)
+        {
+            _showConfirmationModal = false;
+            if (isConfirmed)
+            {
+                if (_operatingCategory != null)
+                {
+                    await DeleteCategoryAsync(_operatingCategory.Id);
+                }
+            }
+        }
+
+        private void CancelDelete()
+        {
+            _showConfirmationModal = false;
         }
     }
 }
