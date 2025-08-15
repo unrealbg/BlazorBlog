25bc98217b2bb625ace375789264d81e5b49a1ad
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-08-15 14:50:17 +0300

    Add CancellationToken support to subscriber methods
    Updated ISubscriberRepository to include optional CancellationToken parameters in AddSubscriberAsync and GetSubscribersAsync methods. Modified SubscriberRepository to implement these changes, enhancing control over asynchronous operations and allowing for cancellation of long-running database tasks.

--- BlazorBlog.Repository/Contracts/ISubscriberRepository.cs
+++ BlazorBlog.Repository/Contracts/ISubscriberRepository.cs
@@ -1,11 +1,14 @@
 ﻿namespace BlazorBlog.Repository.Contracts
 {
+    using System.Threading;
+
     using Data.Entities;
     using Data.Models;
 
     public interface ISubscriberRepository
     {
-        Task<string?> AddSubscriberAsync(Subscriber subscriber);
-        Task<PageResult<Subscriber>> GetSubscribersAsync(int startIndex, int pageSize);
+        Task<string?> AddSubscriberAsync(Subscriber subscriber, CancellationToken cancellationToken = default);
+
+        Task<PageResult<Subscriber>> GetSubscribersAsync(int startIndex, int pageSize, CancellationToken cancellationToken = default);
     }
 }
